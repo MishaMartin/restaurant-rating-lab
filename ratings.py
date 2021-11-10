237e484e1ca7b3c52ca7a0f78e502d8151b8eefa
Misha Martin <mishanicolemartin@gmail.com>
2021-11-10 15:54:56 -0700

    Final commits

--- ratings.py
+++ ratings.py
@@ -1,4 +1,27 @@
 """Restaurant rating lister."""
+scores = open("scores.txt", "r")
+scores = scores.readlines()
+scores.sort()
 
+dictionary = {}
 
-# put your code here
+for i in scores:
+    i = i.rstrip().split(":")
+    dictionary[F"{i[0]}"] = F"{i[1]}"
+print(dictionary)
+
+for i in dictionary:
+    print(i,"is rated at", dictionary[i])
+
+print("We would love for you to rate a new restaurant for our list!")
+new_restaurant = str(input("Restarant Name\n"))
+new_rating = str(input("Restaurant Rating\n"))
+
+dictionary[new_restaurant] = new_rating
+
+sorted_dictionary = {}
+for i in sorted(dictionary):
+    sorted_dictionary[i] = dictionary[i]
+
+for i in sorted_dictionary:
+    print(i,"is rated at", sorted_dictionary[i])
